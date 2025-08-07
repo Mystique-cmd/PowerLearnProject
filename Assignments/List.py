3facf2b8ace1f33fb23d8a513748f1f31a72cd04
Mystique <mystiquesoul777@gmail.com>
2025-08-07 10:41:15 +0300

    Lists

---
+++ Assignments/List.py
@@ -0,0 +1,15 @@
+#an empty list
+my_list = []
+#Appending elements to the list
+my_list.append(10, 20, 30, 40)
+#Inserting an element at a specific position
+my_list.insert(1, 15)
+#Extending the list with multiple elements
+my_list.extend([50, 60, 70])
+#Removing the last element
+my_list.pop()
+#Sorting the list
+my_list.sort()
+#Finding the index of an element and printing it
+index_30 = my_list.index(30)
+print("Index of 30:",index_30)
